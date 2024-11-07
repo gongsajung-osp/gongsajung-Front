8e25f3f0360f9c874aad2a506492d5e315162f8a
methal <handsson@ewhain.net>
2024-11-08 00:47:03 +0900

    modify app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return render_template("item.html")
+    return render_template("login.html")
 
 @application.route("/index")
 def view_index():
@@ -34,27 +34,30 @@ def reg_item():
 def reg_review():
     return render_template("reg_reviews.html")
 
+@application.route("/history")
+def view_history():
+    return render_template("history.html")
+
 @application.route("/submit_item")
 def reg_item_submit():
-    name = request.args.get("name")
-    seller = request.args.get("seller")
-    addr = request.args.get("addr")
-    email = request.args.get("email")
-    category = request.args.get("category")
-    card = request.args.get("card")
-    status = request.args.get("status")
-    phone = request.args.get("phone")
-    print(name, seller, addr, email, category, card, status, phone)
-    # 우리는 main/sub category 두 개가 있으니 추가로 넘겨줘야함
-    # 여기는 왜 price가 없냐
+    name=request.args.get("name")
+    seller=request.args.get("seller")
+    addr=request.args.get("addr")
+    email=request.args.get("email")
+    category=request.args.get("category")
+    card=request.args.get("card")
+    status=request.args.get("status")
+    phone=request.args.get("phone")
+    print(name,seller,addr,email,category,card,status,phone)
+#main/sub category, prcie 추가로 넘겨야 함
 
 @application.route("/submit_item_post", methods=['POST'])
 def reg_item_submit_post():
-    image_file = request.files["file"]
+    image_file=request.files["file"]
     image_file.save("static/images/{}".format(image_file.filename))
     
-    data = request.form
+    data=request.form
     return render_template("submit_item_result.html", data=data, img_path="static/images/{}".format(image_file.filename))
 
-if __name__ == "__main__":
-    application.run(debug=True, host='0.0.0.0')
+if __name__=="__main__":
+    application.run(host='0.0.0.0',debug=True)
